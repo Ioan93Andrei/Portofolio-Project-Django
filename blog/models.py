be11851d94fe80509834bde29a237b9f584bea8b
Ioan-Andrei Ojica <ivan.andrei93@mail.ru>
2021-01-05 16:30:27 +0100

    added the blog model and the views accordingly.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.db.models.fields import CharField
 
-# Create your models here.
+class Blog(models.Model):
+  title = models.CharField(max_length=250)
+  date = models.DateField(auto_now=False, auto_now_add=False)
+  content = models.TextField(max_length=1000)
+  image = models.ImageField(upload_to="blog/images/", blank=True)
